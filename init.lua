386916a6ce68466d513b7678b857e4fd88af067b
Adrien Luneau <aluneau@redhat.com>
2025-04-24 15:35:41 +0200

    adding keymap for jumplist

--- init.lua
+++ init.lua
@@ -13,8 +13,9 @@ require('gitsigns').setup()
 require("user.colorscheme")
 require("colorizer").setup({
   user_default_options = {
-    css_fn = false,
+    css_fn = true,
     css = true,
+    tailwind = true,
   },
 })
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
@@ -24,4 +25,4 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
+vim.keymap.set('n', '<leader>fj', builtin.jumplist, { desc = 'Telescope jumplist'})
