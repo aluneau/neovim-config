3477604532ea40ff5258807737ff4f0a7e0fc691
aluneau <aluneau@redhat.com>
2025-05-22 10:26:54 +0200

    add codecompanion

--- init.lua
+++ init.lua
@@ -27,5 +27,5 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>fj', builtin.jumplist, { desc = 'Telescope jumplist'})
-
+vim.keymap.set("n", "<leader>cc", "<cmd>CodeCompanion<cr>", { desc = "Code Companion Chat" })
 
---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,50 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  cmd = { "CodeCompanion" },
+  config = function()
+  require("codecompanion").setup({
+    adapters = {
+      ollama_remote = function()
+        return require("codecompanion.adapters").extend("ollama", {
+          name = "ollama_remote",
+          env = {
+            url = "http://127.0.0.1:11434",
+          },
+          headers = {
+            ["Content-Type"] = "application/json",
+          },
+          parameters = {
+            sync = true,
+          },
+          schema = {
+            model = {
+              default = "mistral",
+            },
+            num_ctx = {
+              default = 16384,
+            },
+            num_predict = {
+              default = -1,
+            },
+          },
+        })
+      end,
+    },
+    strategies = {
+      chat = {
+        adapter = "ollama_remote",
+      },
+      inline = {
+        adapter = "ollama_remote",
+      },
+      agent = {
+        adapter = "ollama_remote",
+      },
+    },
+  })
+  end,
+}
